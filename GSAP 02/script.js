3c7cd56ed46b7b23bb4009acbfe28935070ad61a
aisarthak <aisarthak2@gamil.com>
2023-11-10 11:59:31 +0530

    005

---
+++ GSAP 02/script.js
@@ -0,0 +1,20 @@
+gsap.from("#page1 #box1", {
+  scale: 0,
+  delay: 1,
+  duration: 2,
+  rotate: 360,
+});
+gsap.from("#page2 #box2", {
+  scale: 0,
+  delay: 1,
+  duration: 2,
+  rotate: 360,
+  scrollTrigger: {
+    trigger: "#page2 #box2",
+    scroller: "body",
+    markers:true,
+    start:"top 50%",
+    end:"top 30%",
+    scrub:3,  //0 to 5 gives smoothness 
+  },
+});
